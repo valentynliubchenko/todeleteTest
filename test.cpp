aba55a3043039e2ecc061b7ea4a7d5b0be6ed36d
valentynliubchenko <lubchenko.val@gmail.com>
2025-03-09 22:22:00 +0200

    test

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by DELL on 09.03.2025.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,16 @@
+//
+// Created by DELL on 09.03.2025.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //TEST_H
